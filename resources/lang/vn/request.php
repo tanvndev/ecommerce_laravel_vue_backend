fffe7f3b33951fa1ce7b805863b7343b309b6863
TanVu <tanvnph33606@gmail.com>
2024-06-23 16:47:43 +0700

    Update Login

---
+++ resources/lang/vn/request.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'messages' => [
+        'required' => ':attribute không được để trống.',
+        'unique' => ':attribute đã tồn tại.',
+        'email' => ':attribute không đúng định dạng email.',
+        'string' => ':attribute phải là chuỗi ký tự.',
+        'integer' => ':attribute phải là số nguyên.',
+        'min' => ':attribute phải có ít nhất :min ký tự.',
+        'same' => ':attribute phải trùng khớp với :other.',
+        'gt' => ':attribute bắt buộc phải chọn.',
+        'after' => ':attribute phải sau ngày :date.',
+    ],
+
+];
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\V1\{
+    AuthController
+};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::post('/v1/auth/login', [AuthController::class, 'login']);
